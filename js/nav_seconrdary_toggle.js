38a0c759807a7e530cb607c1b641b082203a7369
Gilmielle <bizarre_princes@mail.ru>
2021-05-30 22:26:27 +0500

    corrections to header

--- js/nav_seconrdary_toggle.js
+++ js/nav_seconrdary_toggle.js
@@ -1,5 +1,29 @@
 window.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#dropdown_btn_1').addEventListener('click', function() {
     document.querySelector('#scroll-container_1').classList.toggle('active')
-  })
+  });
+})
+
+window.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('#dropdown_btn_2').addEventListener('click', function() {
+    document.querySelector('#scroll-container_2').classList.toggle('active')
+  });
+})
+
+window.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('#dropdown_btn_3').addEventListener('click', function() {
+    document.querySelector('#scroll-container_3').classList.toggle('active')
+  });
+})
+
+window.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('#dropdown_btn_4').addEventListener('click', function() {
+    document.querySelector('#scroll-container_4').classList.toggle('active')
+  });
+})
+
+window.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('#dropdown_btn_5').addEventListener('click', function() {
+    document.querySelector('#scroll-container_5').classList.toggle('active')
+  });
 })
