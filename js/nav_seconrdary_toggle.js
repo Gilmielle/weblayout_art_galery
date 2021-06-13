159eef8e3b3bc5cab9b70d48623c7602905ac803
Gilmielle <bizarre_princes@mail.ru>
2021-06-13 19:10:16 +0500

    corrections of header dropdowns

--- js/nav_seconrdary_toggle.js
+++ js/nav_seconrdary_toggle.js
@@ -1,29 +1,79 @@
 window.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('#dropdown_btn_1').addEventListener('click', function() {
-    document.querySelector('#scroll-container_1').classList.toggle('active')
+  document.querySelector('#dropdown__btn_1').addEventListener('click', function() {
+    document.querySelector('#scroll-container_1').classList.toggle('scroll-container_active');
+    document.querySelector('#scroll-container_2').classList.remove('scroll-container_active');
+    document.querySelector('#scroll-container_3').classList.remove('scroll-container_active');
+    document.querySelector('#scroll-container_4').classList.remove('scroll-container_active');
+    document.querySelector('#scroll-container_5').classList.remove('scroll-container_active');
+
+    document.querySelector('#dropdown__icon_1').classList.toggle('dropdown__icon_active');
+    document.querySelector('#dropdown__icon_2').classList.remove('dropdown__icon_active');
+    document.querySelector('#dropdown__icon_3').classList.remove('dropdown__icon_active');
+    document.querySelector('#dropdown__icon_4').classList.remove('dropdown__icon_active');
+    document.querySelector('#dropdown__icon_5').classList.remove('dropdown__icon_active');  
   });
 })
 
 window.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('#dropdown_btn_2').addEventListener('click', function() {
-    document.querySelector('#scroll-container_2').classList.toggle('active')
+  document.querySelector('#dropdown__btn_2').addEventListener('click', function() {
+    document.querySelector('#scroll-container_1').classList.remove('scroll-container_active');
+    document.querySelector('#scroll-container_2').classList.toggle('scroll-container_active');
+    document.querySelector('#scroll-container_3').classList.remove('scroll-container_active');
+    document.querySelector('#scroll-container_4').classList.remove('scroll-container_active');
+    document.querySelector('#scroll-container_5').classList.remove('scroll-container_active');
+
+    document.querySelector('#dropdown__icon_1').classList.remove('dropdown__icon_active');
+    document.querySelector('#dropdown__icon_2').classList.toggle('dropdown__icon_active');
+    document.querySelector('#dropdown__icon_3').classList.remove('dropdown__icon_active');
+    document.querySelector('#dropdown__icon_4').classList.remove('dropdown__icon_active');
+    document.querySelector('#dropdown__icon_5').classList.remove('dropdown__icon_active');  
   });
 })
 
 window.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('#dropdown_btn_3').addEventListener('click', function() {
-    document.querySelector('#scroll-container_3').classList.toggle('active')
+  document.querySelector('#dropdown__btn_3').addEventListener('click', function() {
+    document.querySelector('#scroll-container_1').classList.remove('scroll-container_active');
+    document.querySelector('#scroll-container_2').classList.remove('scroll-container_active');
+    document.querySelector('#scroll-container_3').classList.toggle('scroll-container_active');
+    document.querySelector('#scroll-container_4').classList.remove('scroll-container_active');
+    document.querySelector('#scroll-container_5').classList.remove('scroll-container_active');
+
+    document.querySelector('#dropdown__icon_1').classList.remove('dropdown__icon_active');
+    document.querySelector('#dropdown__icon_2').classList.remove('dropdown__icon_active');
+    document.querySelector('#dropdown__icon_3').classList.toggle('dropdown__icon_active');
+    document.querySelector('#dropdown__icon_4').classList.remove('dropdown__icon_active');
+    document.querySelector('#dropdown__icon_5').classList.remove('dropdown__icon_active');  
   });
 })
 
 window.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('#dropdown_btn_4').addEventListener('click', function() {
-    document.querySelector('#scroll-container_4').classList.toggle('active')
+  document.querySelector('#dropdown__btn_4').addEventListener('click', function() {
+    document.querySelector('#scroll-container_1').classList.remove('scroll-container_active');
+    document.querySelector('#scroll-container_2').classList.remove('scroll-container_active');
+    document.querySelector('#scroll-container_3').classList.remove('scroll-container_active');
+    document.querySelector('#scroll-container_4').classList.toggle('scroll-container_active');
+    document.querySelector('#scroll-container_5').classList.remove('scroll-container_active');
+
+    document.querySelector('#dropdown__icon_1').classList.remove('dropdown__icon_active');
+    document.querySelector('#dropdown__icon_2').classList.remove('dropdown__icon_active');
+    document.querySelector('#dropdown__icon_3').classList.remove('dropdown__icon_active');
+    document.querySelector('#dropdown__icon_4').classList.toggle('dropdown__icon_active');
+    document.querySelector('#dropdown__icon_5').classList.remove('dropdown__icon_active');  
   });
 })
 
 window.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('#dropdown_btn_5').addEventListener('click', function() {
-    document.querySelector('#scroll-container_5').classList.toggle('active')
+  document.querySelector('#dropdown__btn_5').addEventListener('click', function() {
+    document.querySelector('#scroll-container_1').classList.remove('scroll-container_active');
+    document.querySelector('#scroll-container_2').classList.remove('scroll-container_active');
+    document.querySelector('#scroll-container_3').classList.remove('scroll-container_active');
+    document.querySelector('#scroll-container_4').classList.remove('scroll-container_active');
+    document.querySelector('#scroll-container_5').classList.toggle('scroll-container_active');
+
+    document.querySelector('#dropdown__icon_1').classList.remove('dropdown__icon_active');
+    document.querySelector('#dropdown__icon_2').classList.remove('dropdown__icon_active');
+    document.querySelector('#dropdown__icon_3').classList.remove('dropdown__icon_active');
+    document.querySelector('#dropdown__icon_4').classList.remove('dropdown__icon_active');
+    document.querySelector('#dropdown__icon_5').classList.toggle('dropdown__icon_active');  
   });
 })
