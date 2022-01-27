b446f31380a37b3bea6a10684f8b54a3393b5f34
Gilmielle <bizarre_princes@mail.ru>
2022-01-27 15:46:39 +0500

    little corrections of header; image weight reduction, images structured; promo block added

--- js/burger.js
+++ js/burger.js
@@ -1,8 +1,10 @@
 window.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#burger').addEventListener('click', function() {
     document.querySelector('#menu').classList.add('is-active');
+    document.querySelector("body").disableScroll.off();
   });
   document.querySelector('#exit').addEventListener('click', function() {
     document.querySelector('#menu').classList.remove('is-active');
+    document.querySelector("body").disableScroll.on();
   });
 })
